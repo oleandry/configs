1fff2684fb7f0721a0a82c2c28362835b35cc783
Ole Andreas Rydland <ole.andreas.rydland@if.no>
2017-01-03 15:57:25 +0100

    Seperated choco install into seperate files

---
+++ new-box-development.bat
@@ -0,0 +1,7 @@
+choco install git.install -y
+choco install conemu -y
+choco install nuget.commandline -y
+choco install poshgit -y
+choco install visualstudiocode -y
+choco install beyondcompare -y
+choco install git-credential-manager-for-windows -y
