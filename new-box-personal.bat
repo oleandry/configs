be3794ee10fff1e2e2ae64542c9e1d13fc2ff99d
Ole Andreas Rydland <ole.andreas.rydland@if.no>
2017-04-06 11:44:54 +0200

    updated

---
+++ new-box-personal.bat
@@ -0,0 +1,2 @@
+choco install spotify -y
+choco install vlc -y
--- new-box.bat
+++ new-box.bat
@@ -1,13 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco install googlechrome -y
 choco install 7zip.install -y
-choco install git.install -y
-choco install conemu -y
-choco install nuget.commandline -y
-choco install poshgit -y
-choco install visualstudiocode -y
-choco install spotify -y
-choco install vlc -y
-choco install beyondcompare -y
 choco install slack -y
-choco install git-credential-manager-for-windows -y
+
